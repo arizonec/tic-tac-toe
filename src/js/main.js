c6219914b01306047ea3ed8ad28e2b865979aa83
Kirill Kolesnikov <116028314+arizonec@users.noreply.github.com>
2023-09-10 22:28:48 +0300

    arrs include

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,9 @@ const items = document.querySelectorAll('.cell');
 const modal = document.querySelector('.modal');
 const modalWindow = document.querySelector('.modal__window');
 const resetBtn = document.querySelector('.reset');
+const playerBtn = document.querySelector('.player');
+const botBtn = document.querySelector('.bot');
 
-let movePlayer = true;
 const winCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -19,34 +20,51 @@ const winCombinations = [
     [2, 4, 6],
 ]
 
+let mode = true;
+let movePlayer = true;
 let first = [];
 let second = [];
+let bot = [];
 
 
 field.addEventListener('click', ({ target }) => {
     if (target.classList.contains('cell')) {
         let collection = document.querySelectorAll(".cell:not(.active)");
-        if (collection.length == 1) {
+        if (collection.length == 1 && !found) {
             modal.style.visibility = 'visible';
             modalWindow.innerHTML = `нет победителя`;
         }
 
-        if (!target.classList.contains("active")) {
+        if (!target.classList.contains("active") && mode) {
             if (movePlayer) {
                 firstPlayer(target);
             } else {
                 secondPlayer(target);
             }
         }
+
+
+        if (!target.classList.contains("active") && !mode) {
+            if (movePlayer) {
+                firstPlayer(target);
+            }
+            setTimeout(() => botAction(), 1000);
+        }
+
     }
 
 })
 
-function firstPlayer(that) {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+const firstPlayer = (that) => {
     if (!that.classList.contains('active')) {
         that.classList.add("active");
         that.classList.add("active_x");
-        first.push(that.id);
+        first.push(Number(that.id));
+        console.log(first);
         checkMap(first);
     }
 
@@ -55,30 +73,51 @@ function firstPlayer(that) {
     movePlayer = !movePlayer;
 }
 
-function secondPlayer(that) {
+const secondPlayer = (that) => {
     if (!that.classList.contains('active')) {
         that.classList.add("active");
         that.classList.add("active_o");
-        second.push(that.id)
+        second.push(Number(that.id))
         checkMap(second);
     }
 
     movePlayer = !movePlayer;
 }
 
+const botAction = () => {
+    let collection = document.querySelectorAll(".cell:not(.active)");
+    if (collection.length > 0) {
+        let step = getRandomInt(collection.length);
+        if (!collection[step].classList.contains('active')) {
+            collection[step].classList.add("active");
+            collection[step].classList.add("active_o");
+            bot.push(Number(collection[step].id));
+            checkMap(bot);
+            console.log(bot);
+            movePlayer = !movePlayer;
+            collection = document.querySelectorAll(".cell:not(.active)");
+        }
+    }
+}
+let found = false;
+const checkMap = (arr) => {
+    // let found = false;
 
-function checkMap(arr) {
-    let found = false;
     for (let i = 0; i < winCombinations.length; i++) {
-        if (winCombinations[i].sort().toString() === arr.sort().toString()) {
+        // if (arr.sort().toString().includes(winCombinations[i].sort().toString())) {
+        //     found = true;
+        // }
+        if (winCombinations[i].every(x => arr.includes(x))) {
             found = true;
         }
+        //разбить по элементам массива и сравнивать по одному друг с другом
+
+
     }
     if (found) {
         stop(arr);
     }
 }
-
 
 const stop = (arr) => {
     modal.style.visibility = 'visible';
@@ -90,21 +129,36 @@ const stop = (arr) => {
 }
 
 const reset = () => {
+    bot = [];
     first = [];
     second = [];
     items.forEach(item => item.classList.remove('active', 'active_o', 'active_x'));
     movePlayer = true;
+    found = false;
 }
 
 const closeModal = ({ target }) => {
     if (target.classList.contains('modal')) {
         modal.style.visibility = 'hidden';
-        console.log(first)
+        bot = [];
+        first = [];
+        second = [];
+        items.forEach(item => item.classList.remove('active', 'active_o', 'active_x'));
+        movePlayer = true;
+        found = false;
     }
 }
 
 document.addEventListener('click', closeModal);
 resetBtn.addEventListener('click', reset);
+playerBtn.addEventListener('click', () => {
+    mode = true;
+})
+
+botBtn.addEventListener('click', () => {
+    mode = false;
+})
+
 
 
 
